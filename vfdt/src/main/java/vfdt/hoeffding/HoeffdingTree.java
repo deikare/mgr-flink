2c73ae3b2170b42c6acb8b619c9c324769ee8207
deikare <bbgolawski@gmail.com>
2023-01-05 05:40:26 +0100

    Add method for counting highest heuristic attributes

--- vfdt/src/main/java/vfdt/hoeffding/HoeffdingTree.java
+++ vfdt/src/main/java/vfdt/hoeffding/HoeffdingTree.java
@@ -1,7 +1,10 @@
 package vfdt.hoeffding;
 
+import org.apache.flink.api.java.tuple.Tuple4;
+
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
 public class HoeffdingTree <K, S extends NodeStatistics<K>, C extends ComparatorInterface<K>, B extends StatisticsBuilderInterface<K, S>> {
@@ -9,22 +12,25 @@ public class HoeffdingTree <K, S extends NodeStatistics<K>, C extends Comparator
     private long nMin;
     private int R;
     private double delta;
-    private HashSet<String> attributes;
+    private HashSet<String> attributes; //TODO separate attributes on those which are continuous and discrete - this way e.g. decorator pattern should be used in branching instead of getChildIndex from ComparatorInterface
     private HashSet<String> classLabels;
     private double tau;
     private Node<K, S, C> root;
     private B statisticsBuilder;
     private BiFunction<String, Node<K, S, C>, Double> heuristic;
 
+    private BiConsumer<Example<K>, Node<K, S, C>> nodeSplitter; //TODO consider using attribute instead of example
+
     public HoeffdingTree() {
     }
 
-    public HoeffdingTree(int R, double delta, HashSet<String> attributes, HashSet<String> classLabels, double tau, long nMin, B statisticsBuilder, BiFunction<String, Node<K, S, C>, Double> heuristic) {
+    public HoeffdingTree(int R, double delta, HashSet<String> attributes, HashSet<String> classLabels, double tau, long nMin, B statisticsBuilder, BiFunction<String, Node<K, S, C>, Double> heuristic, BiConsumer<Example<K>, Node<K, S, C>> nodeSplitter) {
         this.R = R;
         this.delta = delta;
         this.attributes = attributes;
         this.classLabels = classLabels;
         this.tau = tau;
+        this.nodeSplitter = nodeSplitter;
         this.n = 0;
         this.nMin = nMin;
         this.statisticsBuilder = statisticsBuilder;
@@ -44,29 +50,16 @@ public class HoeffdingTree <K, S extends NodeStatistics<K>, C extends Comparator
             n += 1;
 
             if (leaf.getStatistics().getN() > nMin) {
-                String xa = null;
-                String xb = null;
-                Double hXa = null;
-                Double hXb = null;
-                for (String attribute : attributes) {
-                    Double h = heuristic.apply(attribute, leaf);
-                    if (xa == null || h > hXa)
-                    {
-                        xa = attribute;
-                        hXa = h;
-                    }
-                    else if (xb == null || h > hXb) {
-                        xb = attribute;
-                        hXb = h;
-                    }
-                }
+                Tuple4<String, Double, String, Double> tuple4 = twoAttributesWithLargestHeuristic(leaf);
 
                 double eps = getEpsilon();
-                if (hXa != null && hXb != null && hXa - hXb > eps) {
+                if (tuple4.f1 != null && tuple4.f3 != null && tuple4.f1 - tuple4.f3 > eps) {
+                    nodeSplitter.accept(example, leaf);
                     //split
                     //init new leafs
                 }
                 else if (eps < tau) {
+                    nodeSplitter.accept(example, leaf);
                     //split
                     //init new leafs
                 }
@@ -84,6 +77,27 @@ public class HoeffdingTree <K, S extends NodeStatistics<K>, C extends Comparator
 
     private double getEpsilon() {
         return Math.sqrt(Math.pow(R, 2) * Math.log(2/delta) / (2 * n));
+    }
+
+    private Tuple4<String, Double, String, Double> twoAttributesWithLargestHeuristic(Node<K, S, C> node) {
+        String xa = null;
+        String xb = null;
+        Double hXa = null;
+        Double hXb = null;
+        for (String attribute : attributes) {
+            Double h = heuristic.apply(attribute, node);
+            if (xa == null || h > hXa)
+            {
+                xa = attribute;
+                hXa = h;
+            }
+            else if (xb == null || h > hXb) {
+                xb = attribute;
+                hXb = h;
+            }
+        }
+
+        return new Tuple4<>(xa, hXa, xb, hXb);
     }
 
 
