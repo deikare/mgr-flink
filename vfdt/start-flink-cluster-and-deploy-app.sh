d5d2a83a0567e24d9c9730acdf2b7a0a08511b4a
deikare <bbgolawski@gmail.com>
2023-11-16 13:36:00 +0100

    Add todo

--- vfdt/start-flink-cluster-and-deploy-app.sh
+++ vfdt/start-flink-cluster-and-deploy-app.sh
@@ -2,6 +2,8 @@
 
 http_status=$(curl -s -o /dev/null -w "%{http_code}" "${FLINK_ADDRESS}")
 
+#todo read from maven steps name of created jar instead of hardcoding it as env $JAR_FILE
+
 if [ "${http_status}" != "200" ]; then
   "${FLINK_BIN_PATH}"/start-cluster.sh
 fi
