c7cc811db8190fdd55d70c72f110a240937f822f
deikare <bbgolawski@gmail.com>
2024-01-08 14:14:45 +0100

    Add base files

---
+++ vfdt/src/main/java/vfdt/classifiers/base/BaseClassifierStraight.java
@@ -0,0 +1,29 @@
+package vfdt.classifiers.base;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import vfdt.inputs.Example;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+
+public abstract class BaseClassifierStraight extends BaseClassifier implements Serializable {
+    public Tuple2<String, HashMap<String, Long>> train(Example example) {
+        Instant start = Instant.now();
+        HashMap<String, Long> trainingPerformance = trainImplementation(example);
+        trainingPerformance.put(BaseClassifierFields.TRAINING_DURATION, toNow(start));
+        return new Tuple2<>(timestampTrailingZeros(start), trainingPerformance);
+    }
+
+    public Tuple2<String, HashMap<String, Long>> classify(Example example, HashMap<String, Long> performances) {
+        Instant start = Instant.now();
+        Tuple2<String, HashMap<String, Long>> predictionResult = classifyImplementation(example, performances);
+        predictionResult.f1.put(BaseClassifierFields.CLASSIFICATION_DURATION, toNow(start));
+        return predictionResult;
+    }
+
+    protected abstract HashMap<String, Long> trainImplementation(Example example);
+
+    protected abstract Tuple2<String, HashMap<String, Long>> classifyImplementation(Example example, HashMap<String, Long> performances);
+}
