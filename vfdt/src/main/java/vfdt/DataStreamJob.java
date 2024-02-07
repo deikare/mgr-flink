e67d2958f1b0dbbf2447b4edd92ab1b578995034
deikare <bbgolawski@gmail.com>
2024-02-07 15:53:35 +0100

    Tune extended Dwm

--- vfdt/src/main/java/vfdt/DataStreamJob.java
+++ vfdt/src/main/java/vfdt/DataStreamJob.java
@@ -184,7 +184,7 @@ public class DataStreamJob {
                     protected ExtendedDynamicWeightedMajority<GaussianNaiveBayesClassifier> createClassifier() {
                         int classNumber = decoder.size();
                         double beta = 0.5;
-                        double threshold = 0.01;
+                        double threshold = 0.12;
                         int updateClassifiersEachSamples = 10;
 
                         return new ExtendedDynamicWeightedMajority<GaussianNaiveBayesClassifier>(beta, threshold, classNumber, updateClassifiersEachSamples) {
