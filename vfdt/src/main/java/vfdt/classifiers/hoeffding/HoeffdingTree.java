df536fd14198c4c6958a8c470511b6f5b1048d72
deikare <bbgolawski@gmail.com>
2024-02-08 14:22:21 +0100

    Tune VFDT

--- vfdt/src/main/java/vfdt/classifiers/hoeffding/HoeffdingTree.java
+++ vfdt/src/main/java/vfdt/classifiers/hoeffding/HoeffdingTree.java
@@ -172,7 +172,7 @@ public abstract class HoeffdingTree<N_S extends NodeStatistics, B extends Statis
     }
 
     private double getEpsilon() {
-        return Math.sqrt(Math.pow(R, 2) * Math.log(2 / delta) / (2 * n));
+        return ((double) R) * Math.sqrt(Math.log(2 / delta) / (2 * n));
     }
 
     private class HighestHeuristicPOJO {
