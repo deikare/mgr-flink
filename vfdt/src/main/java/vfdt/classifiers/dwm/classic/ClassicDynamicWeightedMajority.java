e4c8ccb1e6f0b6d869c77f91de8bdf18022ed647
deikare <bbgolawski@gmail.com>
2024-02-22 16:31:43 +0100

    Unify train DWM

--- vfdt/src/main/java/vfdt/classifiers/dwm/classic/ClassicDynamicWeightedMajority.java
+++ vfdt/src/main/java/vfdt/classifiers/dwm/classic/ClassicDynamicWeightedMajority.java
@@ -1,16 +1,7 @@
 package vfdt.classifiers.dwm.classic;
 
-import org.apache.flink.api.java.tuple.Tuple2;
 import vfdt.classifiers.base.BaseDynamicWeightedMajority;
 import vfdt.classifiers.base.ClassifierPojo;
-import vfdt.classifiers.dwm.DwmClassifierFields;
-import vfdt.classifiers.helpers.Helpers;
-import vfdt.inputs.Example;
-
-import java.time.Instant;
-import java.util.ArrayList;
-
-import static vfdt.classifiers.helpers.Helpers.getIndexOfHighestValue;
 
 public abstract class ClassicDynamicWeightedMajority<C extends ClassifierInterface> extends BaseDynamicWeightedMajority<C, ClassifierPojo<C>> {
 
@@ -19,36 +10,13 @@ public abstract class ClassicDynamicWeightedMajority<C extends ClassifierInterfa
     }
 
     @Override
-    protected ArrayList<Tuple2<String, Long>> trainImplementation(Example example, int predictedClass, ArrayList<Tuple2<String, Long>> performances) {
-        if (sampleNumber % updateClassifiersEachSamples == 0) {
-            ArrayList<Tuple2<String, Long>> normalizationAndDeletePerformances = normalizeWeightsAndDeleteClassifiersWithWeightUnderThreshold();
-            if (predictedClass != example.getMappedClass()) {
-                Instant start = Instant.now();
-                classifiersPojo.add(createClassifierWithWeight(sampleNumber));
-                normalizationAndDeletePerformances.add(Tuple2.of(DwmClassifierFields.ADD_CLASSIFIER_DURATION, Helpers.toNow(start)));
-                normalizationAndDeletePerformances.add(Tuple2.of(DwmClassifierFields.ADDED_CLASSIFIERS_COUNT, 1L));
-            }
-            performances.addAll(normalizationAndDeletePerformances);
-        }
+    protected boolean shouldNormalizeWeightsAndDeleteClassifiers() {
+        return sampleNumber % updateClassifiersEachSamples == 0;
+    }
 
-        ArrayList<Tuple2<String, Long>> avgLocalPerformances = new ArrayList<>();
-        for (int classifierIndex = 0; classifierIndex < classifiersPojo.size(); classifierIndex++) {
-            //czyli trzeba jak obcinamy wagę, to sortować w odpowiedniej kolejności klasyfikator
-            //w ten sposób max będzie zawsze z przodu po przejściu wszystkich
-            ClassifierPojo<C> classifierAndWeight = classifiersPojo.get(classifierIndex);
-            ArrayList<Tuple2<String, Long>> localClassifierPerformances = classifierAndWeight.train(example);
+    @Override
+    protected void normalizeWeightsAndDeleteClassifiersSideEffects() {
 
-            updateGlobalWithLocalPerformances(localClassifierPerformances, avgLocalPerformances);
-
-            classifiersPojo.set(classifierIndex, classifierAndWeight);
-        }
-
-        averagePerformanceByLocalClassifier(avgLocalPerformances, classifiersPojo.size());
-        performances.addAll(avgLocalPerformances);
-
-        performances.add(Tuple2.of(DwmClassifierFields.CLASSIFIERS_AFTER_TRAIN_COUNT, (long) classifiersPojo.size()));
-
-        return performances;
     }
 
     @Override
