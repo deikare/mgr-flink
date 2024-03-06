726b557160e8c02b871ad0fab2a391783655144f
deikare <bbgolawski@gmail.com>
2024-03-06 18:53:32 +0100

    Upgrade access modifiers

--- vfdt/src/main/java/vfdt/classifiers/bstHoeffding/bstStatistics/AttributeCountsNode.java
+++ vfdt/src/main/java/vfdt/classifiers/bstHoeffding/bstStatistics/AttributeCountsNode.java
@@ -1,13 +1,13 @@
 package vfdt.classifiers.bstHoeffding.bstStatistics;
 
 public class AttributeCountsNode {
-    public final double value;
+    final double value;
 
-    public long[] ve;
-    public long[] vh;
+    long[] ve;
+    long[] vh;
 
-    public AttributeCountsNode leftChild;
-    public AttributeCountsNode rightChild;
+    AttributeCountsNode leftChild;
+    AttributeCountsNode rightChild;
 
     public AttributeCountsNode(double value, int classIndex, int classNumber) {
         this.value = value;
